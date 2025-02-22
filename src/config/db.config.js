7165b95b52a02348e22274450f75889023170f0d
EliasTG <tg.elias.s@gmail.com>
2025-02-21 22:36:38 -0500

    Change of order directory

--- src/config/db.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    HOST: process.env.DB_HOST || 'your-railway-host',
-    USER: process.env.DB_USER || 'your-username',
-    PASSWORD: process.env.DB_PASSWORD || 'your-password',
-    DATABASE: process.env.DB_NAME || 'your-database',
-    PORT: process.env.DB_PORT || 3306
-};
